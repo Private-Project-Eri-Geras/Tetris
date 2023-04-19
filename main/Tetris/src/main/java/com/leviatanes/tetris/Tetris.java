4e77b4c300acd58394b09ad56855747e7c2b3fef
Eriarer <eriarer_messiah@hotmail.com>
2023-04-19 14:19:50 -0600

    First Commint

---
+++ main/Tetris/src/main/java/com/leviatanes/tetris/Tetris.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.leviatanes.tetris;
+
+/**
+ *
+ * @author Eri
+ */
+public class Tetris {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
