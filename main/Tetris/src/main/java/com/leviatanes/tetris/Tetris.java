13452ecd545c83f14f1a325c9ffb45640e0c8320
Eriarer <eriarer_messiah@hotmail.com>
2023-04-19 14:48:54 -0600

    Cambio de preuba

--- main/Tetris/src/main/java/com/leviatanes/tetris/Tetris.java
+++ main/Tetris/src/main/java/com/leviatanes/tetris/Tetris.java
@@ -11,6 +11,6 @@ package com.leviatanes.tetris;
 public class Tetris {
 
     public static void main(String[] args) {
-        System.out.println("ya se pudo");
+        System.out.println("ya se pudo :D");
     }
 }
