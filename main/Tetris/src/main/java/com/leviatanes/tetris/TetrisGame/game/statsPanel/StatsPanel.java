20b5df903e401ab10feb071e5a1f21a8f54534d2
Eriarer <eriarer_messiah@hotmail.com>
2023-04-25 18:17:21 -0600

    stats panel is stable

--- main/Tetris/src/main/java/com/leviatanes/tetris/TetrisGame/game/statsPanel/StatsPanel.java
+++ main/Tetris/src/main/java/com/leviatanes/tetris/TetrisGame/game/statsPanel/StatsPanel.java
@@ -4,10 +4,7 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-import com.leviatanes.tetris.tetrisGame.game.GameArea;
-
 public class StatsPanel extends javax.swing.JPanel {
-    // private GameArea gameArea;
     /** Label de score */
     private JLabel scoreTextLbl;
     private JLabel scoreLbl1;
@@ -24,7 +21,7 @@ public class StatsPanel extends javax.swing.JPanel {
     private JLabel levelLbl2;
 
     /** nivel del juego */
-    private int level = 0;
+    private int level = 1;
 
     /** Label de lineas */
     private JLabel linesTextLbl;
@@ -54,7 +51,15 @@ public class StatsPanel extends javax.swing.JPanel {
     private final static int levelY = 30;
     private final static int levelW = 4;
     private final static int levelH = 4;
-    //
+
+    // offset para el linesLabel
+    private final static int linesTxtLblX = 3;
+    private final static int linesTxtLblY = 39;
+    // offset para los paneles de lines
+    private final static int linesY = 47;
+    private final static int linesW = 4;
+    private final static int linesH = 4;
+
     private final String folderPath = "/com/leviatanes/tetris/tetrisGame/game/statsPanel/images/";
 
     public StatsPanel(int multiplier) {
@@ -76,6 +81,10 @@ public class StatsPanel extends javax.swing.JPanel {
         return level;
     }
 
+    public int getLines() {
+        return lines;
+    }
+
     // =========[SETTERS]==========
     public void setScore(int score) {
         this.score = score;
@@ -85,8 +94,168 @@ public class StatsPanel extends javax.swing.JPanel {
         this.level = level;
     }
 
-    public void setGameArea(GameArea gameArea) {
-        // this.gameArea = gameArea;
+    public void setLines(int lines) {
+        this.lines = lines;
+    }
+
+    /** Actualiza los labels de score */
+    public void updateScore(int score) {
+        this.score += score;
+        updateScoreLbl();
+    }
+
+    /** Actualiza el label del score */
+    private void updateScoreLbl() {
+        String scoreStr = String.valueOf(score);
+        int socreLength = scoreStr.length();
+        switch (socreLength) {
+            case 1:
+                updateLbl1(scoreStr);
+                break;
+            case 2:
+                updateLbl2(scoreStr);
+                break;
+            case 3:
+                updateLbl3(scoreStr);
+                break;
+            case 4:
+                updateLbl4(scoreStr);
+                break;
+            default:
+                updateLbl5(scoreStr);
+                break;
+        }
+    }
+
+    /** Actualiza el label 1 */
+    private void updateLbl1(String scoreValue) {
+        String imageName;
+        imageName = scoreValue.charAt(0) + ".png";
+
+        System.out.println(scoreValue);
+        setIcon(scoreLbl1, folderPath + imageName, scoreW, scoreH);
+    }
+
+    /** Actualiza el label 2 y los anteriores */
+    private void updateLbl2(String scoreValue) {
+        String imageName;
+        imageName = scoreValue.charAt(0) + ".png";
+        setIcon(scoreLbl2, folderPath + imageName, scoreW, scoreH);
+
+        System.out.println(scoreValue);
+        updateLbl1(scoreValue.charAt(1) + "");
+    }
+
+    /** Actualiza el label 3 y los anteriores */
+    private void updateLbl3(String scoreValue) {
+        String imageName;
+        imageName = scoreValue.charAt(0) + ".png";
+        setIcon(scoreLbl3, folderPath + imageName, scoreW, scoreH);
+
+        System.out.println(scoreValue);
+        updateLbl2(scoreValue.substring(1, scoreValue.length()));
+    }
+
+    /** Actualiza el label 4 y los anterires */
+    private void updateLbl4(String scoreValue) {
+        String imageName;
+        imageName = scoreValue.charAt(0) + ".png";
+        setIcon(scoreLbl4, folderPath + imageName, scoreW, scoreH);
+
+        System.out.println(scoreValue);
+        updateLbl3(scoreValue.substring(1, scoreValue.length()));
+    }
+
+    /** Actualiza el valor del label 5 y los anteriores */
+    private void updateLbl5(String scoreValue) {
+        String imageName;
+        imageName = scoreValue.charAt(0) + ".png";
+        setIcon(scoreLbl5, folderPath + imageName, scoreW, scoreH);
+
+        System.out.println(scoreValue);
+        updateLbl4(scoreValue.substring(1, scoreValue.length()));
+    }
+
+    /** Actualiza los labels de nivel */
+    public void updateLevel(int level) {
+        this.level = level;
+        updateLevelLbl();
+    }
+
+    /** Actualiza los labels de score */
+    private void updateLevelLbl() {
+        String levelStr = String.valueOf(level);
+        int levelLength = levelStr.length();
+        switch (levelLength) {
+            case 1:
+                updateLevelLbl1(levelStr);
+                break;
+            default:
+                updateLevelLbl2(levelStr);
+                break;
+        }
+    }
+
+    /** Actualiza el label 1 */
+    private void updateLevelLbl1(String levelValue) {
+        String imageName;
+        imageName = levelValue.charAt(0) + ".png";
+        setIcon(levelLbl1, folderPath + imageName, levelW, levelH);
+    }
+
+    /** Actualiza el label 2 y los anteriores */
+    private void updateLevelLbl2(String levelValue) {
+        String imageName;
+        imageName = levelValue.charAt(1) + ".png";
+        setIcon(levelLbl2, folderPath + imageName, levelW, levelH);
+        updateLevelLbl1(levelValue.charAt(1) + "");
+    }
+
+    /** Actualiza los labels de lineas */
+    public void updateLines(int lines) {
+        this.lines += lines;
+        updateLinesLbl();
+    }
+
+    /** Actualiza los labels de lineas */
+    private void updateLinesLbl() {
+        String linesStr = String.valueOf(lines);
+        System.out.println(linesStr);
+        int linesLength = linesStr.length();
+        switch (linesLength) {
+            case 1:
+                updateLinesLbl1(linesStr);
+                break;
+            case 2:
+                updateLinesLbl2(linesStr);
+                break;
+            default:
+                updateLinesLbl3(linesStr);
+                break;
+        }
+    }
+
+    /** Actualiza el label 1 */
+    private void updateLinesLbl1(String linesValue) {
+        String imageName;
+        imageName = linesValue.charAt(0) + ".png";
+        setIcon(linesLbl1, folderPath + imageName, linesW, linesW);
+    }
+
+    /** Actualiza el label 2 y los anteriores */
+    private void updateLinesLbl2(String linesValue) {
+        String imageName;
+        imageName = linesValue.charAt(0) + ".png";
+        setIcon(linesLbl2, folderPath + imageName, linesW, linesW);
+        updateLinesLbl1(linesValue.charAt(1) + "");
+    }
+
+    /** Actualiza el label 3 y los anteriores */
+    private void updateLinesLbl3(String linesValue) {
+        String imageName;
+        imageName = linesValue.charAt(0) + ".png";
+        setIcon(linesLbl3, folderPath + imageName, linesW, linesW);
+        updateLinesLbl2(linesValue.substring(1, linesValue.length()));
     }
 
     private void initPanels() {
@@ -94,6 +263,8 @@ public class StatsPanel extends javax.swing.JPanel {
         initScores();
         // inicializar los labels de nivel
         initLevel();
+        // inicializar los labels de lineas
+        initLines();
 
     }
 
@@ -137,7 +308,7 @@ public class StatsPanel extends javax.swing.JPanel {
         // etiquetas con los numeros del nivel
         int levelLblrealX = 12;
         this.levelLbl1 = new JLabel();
-        initLabel(levelLbl1, levelLblrealX, levelY, levelW, levelH, "0.png");
+        initLabel(levelLbl1, levelLblrealX, levelY, levelW, levelH, "1.png");
         levelLblrealX -= 5;
         this.levelLbl2 = new JLabel();
         initLabel(levelLbl2, levelLblrealX, levelY, levelW, levelH, "0.png");
@@ -148,6 +319,28 @@ public class StatsPanel extends javax.swing.JPanel {
 
     }
 
+    /** Inicia lo labels que demuestran las lineas */
+    private void initLines() {
+        // etiqueta con el label de lineas
+        this.linesTextLbl = new JLabel();
+        initLabel(linesTextLbl, linesTxtLblX, linesTxtLblY, scoreTxtLblW, scoreTxtLblH, "Lines.png");
+        // etiquetas con los numeros de lineas
+        int linesLblrealX = 15;
+        this.linesLbl1 = new JLabel();
+        initLabel(linesLbl1, linesLblrealX, linesY, linesW, linesH, "0.png");
+        linesLblrealX -= 5;
+        this.linesLbl2 = new JLabel();
+        initLabel(linesLbl2, linesLblrealX, linesY, linesW, linesH, "0.png");
+        linesLblrealX -= 5;
+        this.linesLbl3 = new JLabel();
+        initLabel(linesLbl3, linesLblrealX, linesY, linesW, linesH, "0.png");
+
+        this.add(linesTextLbl);
+        this.add(linesLbl1);
+        this.add(linesLbl2);
+        this.add(linesLbl3);
+    }
+
     /** Inicializa un label asignandole la imagen correspondiente */
     private void initLabel(JLabel label, int x, int y, int width, int height, String imagePath) {
         x = x * multiplier;
@@ -156,6 +349,12 @@ public class StatsPanel extends javax.swing.JPanel {
         height = height * multiplier;
         label.setBounds(x, y, width, height);
         getIcon(label, folderPath + imagePath, width, height);
+    }
+
+    private void setIcon(JLabel label, String imagePath, int width, int height) {
+        width = width * multiplier;
+        height = height * multiplier;
+        getIcon(label, imagePath, width, height);
     }
 
     private void getIcon(JLabel label, String imagePath, int width, int height) {
