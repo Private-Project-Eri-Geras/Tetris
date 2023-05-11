6dfddb86fb6e46fb8600386df7c7efbaf1a42d52
Eriarer <eriarer_messiah@hotmail.com>
2023-05-11 16:44:17 -0600

    Audio goes smoothli on windows

--- main/Tetris/src/main/java/com/leviatanes/tetris/SoundsPlayer.java
+++ main/Tetris/src/main/java/com/leviatanes/tetris/SoundsPlayer.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,98 +12,192 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+import java.io.BufferedInputStream;
+import com.leviatanes.tetris.soundPlayer.myAudioClip;
 
 public class SoundsPlayer {
-    /** clip de musica principal */
-    private static Clip mainMusic = null;
-    /** control de ganancia de la musica */
-    private static FloatControl gainControl = null;
-    /** bandera de musica silenciada */
-    private static boolean muted = false;
-    /** ganancia anterior */
-    private static float previousGain = 0f;
-    /** private static minimum */
+
+    private static final int MAX_CLIPS = 100;
+    private static int clipIdCounter = 0;
+
+    private static Map<String, myAudioClip> clips = new HashMap<>();
+
+    private static List<myAudioClip> playingClips = new ArrayList<>();
+
     private static float minimum = 0f;
-    /** path de la carpeta de sonidos */
-    private static String soundsPath = "/com/leviatanes/tetris/tetrisGame/game/music/";
-    private static List<Clip> soundClips = new ArrayList<>();
-    private static Map<String, AudioInputStream> soundMap = new HashMap<>();
-    private static List<Thread> hilos = new ArrayList<>();
-    private static AudioInputStream soundStream = null;
-    private static Clip clip = null;
-    private static Thread hilo = null;
-    private static int clipIndex = 0;
+    private static float previousGain = 0f;
+    private static Clip mainMusic = null;
+    private static boolean muted = false;
+    private static FloatControl gainControl = null;
+
+    private static final String soundPath = "/com/leviatanes/tetris/tetrisGame/game/music/";
 
     static {
-        // Inicializamos la lista de clips con 20 elementos
-        for (int i = 0; i < 26; i++) {
-            try {
-                Clip clip = AudioSystem.getClip();
-                soundClips.add(clip);
-                hilos.add(new Thread());
-            } catch (Exception e) {
-                System.out.println("Error al crear el clip: " + e.getMessage());
-            }
-        }
-        String asound = null;
+        loadClip("allClear.wav");
+        loadClip("fall.wav");
+        loadClip("hardDrop.wav");
+        loadClip("highestScore.wav");
+        loadClip("highScore.wav");
+        loadClip("hold.wav");
+        loadClip("levelUp.wav");
+        loadClip("move.wav");
+        loadClip("ok.wav");
+        loadClip("pause.wav");
+        loadClip("results.wav");
+        loadClip("rotate.wav");
+        loadClip("single.wav");
+        loadClip("softDrop.wav");
+        loadClip("tetris.wav");
+        loadClip("triple.wav");
+        loadClip("Tspin.wav");
+    }
+
+    private static void loadClip(String sound) {
         try {
-            for (String sound : Arrays.asList("allClear.wav", "fall.wav", "hardDrop.wav", "highestScore.wav",
-                    "highScore.wav", "hold.wav", "levelUp.wav", "move.wav", "ok.wav", "pause.wav", "results.wav",
-                    "rotate.wav", "single.wav", "softDrop.wav", "tetris.wav", "triple.wav", "Tspin.wav")) {
-                asound = sound;
-                URL audioSrc = SoundsPlayer.class.getResource(soundsPath + sound);
-                AudioInputStream soundStream = AudioSystem.getAudioInputStream(audioSrc);
-                soundStream.mark(Integer.MAX_VALUE); // Marcar el stream para poder resetearlo más tarde
-                soundMap.put(sound, soundStream);
+            // Obtener el archivo de sonido
+            URL soundURL = SoundsPlayer.class.getResource(soundPath + sound);
+            if (soundURL == null) {
+                throw new RuntimeException("No se pudo encontrar el archivo de sonido: " + sound);
             }
+
+            // Cargar el clip de sonido
+            AudioInputStream audioInputStream = AudioSystem
+                    .getAudioInputStream(new BufferedInputStream(soundURL.openStream()));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+
+            // Crear el objeto AudioClip
+            int id = clipIdCounter++ % MAX_CLIPS;
+            myAudioClip audioClip = new myAudioClip(clip, id);
+
+            // Agregar el objeto AudioClip al mapa de clips
+            clips.put(sound, audioClip);
+
         } catch (Exception e) {
-            System.out.println("Error al cargar el sonido " + asound + ": " + e.getMessage());
+            throw new RuntimeException("No se pudo cargar el archivo de sonido: " + sound, e);
         }
     }
 
-    private static void playSound(String sound) {
-        clipIndex = (clipIndex + 1) % soundClips.size();
-        if (soundClips.get(clipIndex).isActive()) {
+    public static void playAllClear() {
+        playSound("allClear.wav");
+    }
+
+    public static void playFall() {
+        playSound("fall.wav");
+    }
+
+    public static void playHardDrop() {
+        playSound("hardDrop.wav");
+    }
+
+    public static void playHighestScore() {
+        playSound("highestScore.wav");
+    }
+
+    public static void playHighScore() {
+        playSound("highScore.wav");
+    }
+
+    public static void playHold() {
+        playSound("hold.wav");
+    }
+
+    public static void playLevelUp() {
+        playSound("levelUp.wav");
+    }
+
+    public static void playMove() {
+        playSound("move.wav");
+    }
+
+    public static void playOk() {
+        playSound("ok.wav");
+    }
+
+    public static void playPause() {
+        playSound("pause.wav");
+    }
+
+    public static void playResults() {
+        playSound("results.wav");
+    }
+
+    public static void playRotate() {
+        playSound("rotate.wav");
+    }
+
+    public static void playSingle() {
+        playSound("single.wav");
+    }
+
+    public static void playSoftDrop() {
+        playSound("softDrop.wav");
+    }
+
+    public static void playTetris() {
+        playSound("tetris.wav");
+    }
+
+    public static void playTriple() {
+        playSound("triple.wav");
+    }
+
+    public static void playTspin() {
+        playSound("Tspin.wav");
+    }
+
+    private static void playSound(String clipName) {
+        myAudioClip audioClip = clips.get(clipName);
+        if (audioClip == null) {
             return;
         }
-        soundClips.get(clipIndex).close();
-        clip = soundClips.get(clipIndex);
-        clip.close();
-        soundStream = soundMap.get(sound);
-        hilo = hilos.get(clipIndex);
-        if (hilo.isAlive()) {
-            hilo.interrupt();
-            clip.close();
-        }
-        hilo = new Thread(() -> {
-            if (soundStream != null) {
-                try {
-                    soundStream.reset();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    clip.open(soundStream);
-                    clip.start();
-                } catch (LineUnavailableException | IOException e) {
-                    System.out.println("Error al cargar el clip " + sound + ": " + e.getMessage());
-                }
-            }
+        // create new thread to play clip
+        Thread thread = new Thread(() -> {
+            try {
+                String path = soundPath + clipName;
+                InputStream audioSrc = SoundsPlayer.class.getResourceAsStream(path);
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioSrc);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
 
+                // add clip to list of playing clips
+                synchronized (playingClips) {
+                    playingClips.add(audioClip);
+                }
+
+                // add listener to remove clip from list when it finishes playing
+                clip.addLineListener(new LineListener() {
+                    @Override
+                    public void update(LineEvent event) {
+                        if (event.getType() == LineEvent.Type.STOP) {
+                            clip.close();
+                            synchronized (playingClips) {
+                                playingClips.remove(audioClip);
+                            }
+                        }
+                    }
+                });
+
+            } catch (IOException | javax.sound.sampled.LineUnavailableException | UnsupportedAudioFileException e) {
+                e.printStackTrace();
+            }
         });
-        hilo.start();
+        thread.start();
     }
 
     public static void playGameMusic() {
         try {
             // carga el archivo de audio
-            String path = soundsPath + "mainTheme.wav";
+            String path = soundPath + "mainTheme.wav";
             InputStream audioSrc = SoundsPlayer.class.getResourceAsStream(path);
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioSrc);
 
             // crea el clip de audio
-            Clip mainMusic = AudioSystem.getClip();
+            mainMusic = AudioSystem.getClip();
             mainMusic.open(audioInputStream);
 
             // control de ganancia
@@ -115,11 +208,9 @@ public class SoundsPlayer {
             mainMusic.setLoopPoints(0, -1); // -1 indica que se repita indefinidamente
             mainMusic.loop(Clip.LOOP_CONTINUOUSLY);
             // establece el valor inicial de ganancia
-            setGain(0.78f);
 
             // inicia la reproducción del clip de audio
             mainMusic.start();
-
         } catch (Exception e) {
             System.out.println("Error al reproducir el archivo de audio: " + e.getMessage());
         }
@@ -215,86 +306,6 @@ public class SoundsPlayer {
                 stopMain();
             }
         }).start();
-    }
-
-    /** reproduce el sonido de mover */
-    public static void playMove() {
-        playSound("move.wav");
-    }
-
-    /** reproduce el sonido de mover para abajo */
-    public static void playSoftDrop() {
-        playSound("softDrop.wav");
-    }
-
-    /** reproduce el sonido de girar */
-    public static void playRotate() {
-        playSound("rotate.wav");
-    }
-
-    /** reproduce el sonido de HardDrop */
-    public static void playHardDrop() {
-        playSound("hardDrop.wav");
-    }
-
-    /** single */
-    public static void playSingle() {
-        playSound("single.wav");
-    }
-
-    /** double */
-    public static void playDouble() {
-        // playSound("double.wav");
-    }
-
-    /** triple */
-    public static void playTriple() {
-        playSound("triple.wav");
-    }
-
-    /** tetris */
-    public static void playTetris() {
-        playSound("tetris.wav");
-    }
-
-    /** tspin */
-    public static void playTspin() {
-        playSound("Tspin.wav");
-    }
-
-    /** Pausa */
-    public static void playPause() {
-        playSound("pause.wav");
-    }
-
-    /** Settle */
-    public static void playFall() {
-        playSound("fall.wav");
-    }
-
-    /** Hold */
-    public static void playHold() {
-        playSound("hold.wav");
-    }
-
-    /** Leveling up */
-    public static void playLevelUp() {
-        playSound("levelUp.wav");
-    }
-
-    /** all clear */
-    public static void playAllClear() {
-        playSound("allClear.wav");
-    }
-
-    /** highestScore */
-    public static void playHighestScore() {
-        playSound("highestScore.wav");
-    }
-
-    /** highScore */
-    public static void playHighScore() {
-        playSound("highScore.wav");
     }
 
 }
