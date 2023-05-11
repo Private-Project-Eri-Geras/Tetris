293350c61aa64222a2fd4f4e628bf52086594835
Eriarer <eriarer_messiah@hotmail.com>
2023-05-11 17:46:16 -0600

    GameControls Fixed

--- main/Tetris/src/main/java/com/leviatanes/tetris/tetrisGame/game/GameControls.java
+++ main/Tetris/src/main/java/com/leviatanes/tetris/tetrisGame/game/GameControls.java
@@ -10,6 +10,7 @@ public class GameControls implements KeyListener {
     private GameArea gameArea;
     private GameThread gameThread;
     private HoldPanel stashShape;
+    private static boolean keyTyped[] = new boolean[256];
 
     public GameControls(GameArea gameArea, GameThread gameThread, HoldPanel stashShape) {
         this.gameArea = gameArea;
@@ -17,26 +18,7 @@ public class GameControls implements KeyListener {
         this.stashShape = stashShape;
     }
 
-    private static final int rotate = 87;
-    private static final int counterRotate = 69;
-    private static final int left = 65;
-    private static final int left2 = 37;
-    private static final int rigth = 68;
-    private static final int rigth2 = 39;
-    private static final int down = 83;
-    private static final int down2 = 40;
-    private static final int shoftDrop = 16;
-    private static final int hardDrop = 32;
-    private static final int pause = 80;
-    private static final int hold = 82;
-    private static final int hold2 = 17;
-    private static final int mute = 77;
-
-    @Override
     /**
-     * Metodo que se ejecuta cuando se presiona una tecla
-     * detectaremos el momento de la pulsacion de las teclas para hacer acciones
-     * 
      * w = rotar bloque (codigo 87)
      * e = contra rotar bloque (codigo 69)
      * a = mover bloque a la izquierda (codigo 65)
@@ -52,11 +34,31 @@ public class GameControls implements KeyListener {
      * control = hacer swap de piezas (codigo 17)
      * m = mutear juego (codigo 77)
      */
+    private static final int rotate = 87;
+    private static final int counterRotate = 69;
+    private static final int left = 65;
+    private static final int left2 = 37;
+    private static final int rigth = 68;
+    private static final int rigth2 = 39;
+    private static final int down = 83;
+    private static final int down2 = 40;
+    private static final int softDrop = 16;
+    private static final int hardDrop = 32;
+    private static final int pause = 80;
+    private static final int hold = 82;
+    private static final int hold2 = 17;
+    private static final int mute = 77;
+
+    @Override
+
     public void keyPressed(KeyEvent key) {
+        if (keyTyped[key.getKeyCode()] == true)
+            return;
         if (this.isPause(key)) // si se pulsa la tecla pausa se pausa el juego
             return;
         if (gameArea.isGameOver() == false) // si el juego no ha terminado se controla el movimiento
             this.controlGame(key);
+        keyTyped[key.getKeyCode()] = true;
     }
 
     @Override
@@ -64,17 +66,14 @@ public class GameControls implements KeyListener {
      * Utilizaremos en caso de ser nesesario detectar una realese
      */
     public void keyReleased(KeyEvent key) {
-        if (key.getKeyCode() == shoftDrop)
+        if (key.getKeyCode() == softDrop)
             gameThread.restoreGameSpeed();
+        keyTyped[key.getKeyCode()] = false;
     }
 
     @Override
     /** Metodo no usado */
     public void keyTyped(KeyEvent key) {
-        if (key.getKeyChar() == 's' || key.getKeyChar() == 'S') {
-            gameThread.resetTime();
-            gameArea.moveDown();
-        }
     }
 
     /**
@@ -99,6 +98,10 @@ public class GameControls implements KeyListener {
             return;
 
         switch (key.getKeyCode()) {
+            case down2:
+            case down:
+                gameArea.moveDown();
+                break;
             case rotate:
                 gameArea.rotate();
                 gameThread.resetSettleTime(); // se resetea el tiempo para bloquear el bloque en el fondo
@@ -111,7 +114,7 @@ public class GameControls implements KeyListener {
             case rigth:
                 gameArea.moveRight();
                 break;
-            case shoftDrop:
+            case softDrop:
                 gameThread.softDrop();
                 break;
             case hardDrop:
