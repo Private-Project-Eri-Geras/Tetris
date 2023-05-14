735a041974d6c8902bf7548403231b035acf36f2
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-05-13 18:39:53 -0600

    comentando autores

---
+++ main/Tetris/src/main/java/com/leviatanes/menus/Mscores.java
@@ -0,0 +1,12 @@
+package com.leviatanes.menus;
+
+import javax.swing.JLabel;
+
+public class Mscores {
+    JLabel titulo;
+
+    public Mscores(){
+        titulo = new JLabel("SCORES");
+    }
+
+}
